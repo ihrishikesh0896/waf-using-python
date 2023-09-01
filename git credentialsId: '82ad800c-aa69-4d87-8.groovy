665bd6e2208b3373e9b2178393d5325dd7dd05d3
devops <devops@localhost>
2023-09-01 23:41:24 +0530

    test1aaa

---
+++ git credentialsId: '82ad800c-aa69-4d87-8.groovy
@@ -0,0 +1,10 @@
+git credentialsId: '82ad800c-aa69-4d87-8014-6f1a42710831', url: 'https://github.com/ihrishikesh0896/waf-using-python.git'
+
+82ad800c-aa69-4d87-8014-6f1a42710831
+
+{AQAAABAAAAAQt2KDlOY6PpFVRcDFhrwS0J5tmK2JWLQjQ5fnbI0hkas=}
+ihrishikesh0896
+
+
+
+
